15272246ab520ca0203bfa45ab05aed359b6b3a4
Matthew Brent <mattjbrent@gmail.com>
2021-05-17 12:54:47 -0400

    small mobile fixes

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ document.getElementById('sidebar-toggle').addEventListener('click', function(e) 
 document.getElementById('mobile-hamburger').addEventListener('click', function(e) {
   document.querySelector('.top-bar').classList.toggle('open');
 });
+
+document.querySelector('.site-title').addEventListener('click', function(e) {
+  document.querySelector('.site-selector').classList.toggle('open');
+});
